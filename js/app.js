3b7a4ba83793806b1a4b35ee3a5df0f1d6a74ea8
Jmfuniilunga <71993621+Jmfuniilunga@users.noreply.github.com>
2020-10-31 19:06:23 +0000

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,12 @@
+
+// Light Box Feature
+
+
+window.addEventListener('load', function() {
+    baguetteBox.run('.gallery');
+});
+
+// Search Filter Function
+
+let search = new Filter('search', 'data-caption');
+
